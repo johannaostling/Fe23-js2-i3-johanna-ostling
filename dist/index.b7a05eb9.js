9cdbbb5d93ebb4a969be90417d4c89a8e3de6f9a
johannaostling <johanna.ostling@gritacademy.se>
2024-03-15 13:56:36 +0100

    working functions

--- dist/index.b7a05eb9.js
+++ dist/index.b7a05eb9.js
@@ -600,8 +600,53 @@ async function searchresult(userinput) {
     const res = await fetch(searchurl);
     const data = await res.json();
     const products = data.products;
-    console.log(products);
+    //   console.log(products);
+    display(products);
 }
+const showingResultDiv = document.querySelector("#showingResultDiv");
+async function display(products) {
+    showingResultDiv.innerHTML = "";
+    console.log(products[0].title);
+    for (const product of products){
+        console.log(product);
+        const img = product.images[0];
+        const title = product.title;
+        const description = product.description;
+        const rating = product.rating;
+        const stock = product.stock;
+        const category = product.category;
+        const imgEl = document.createElement("img");
+        const titelEl = document.createElement("h1");
+        const descriptionEl = document.createElement("p");
+        const ratingEl = document.createElement("p");
+        const stockEl = document.createElement("p");
+        const categoryEl = document.createElement("p");
+        const cartBtn = document.createElement("button");
+        imgEl.src = img;
+        titelEl.innerText = title;
+        descriptionEl.innerText = description;
+        ratingEl.innerText = rating;
+        stockEl.innerText = stock;
+        if (stock <= 10) stockEl.innerText = stock + "(VARNING: few left)";
+        categoryEl.innerText = category;
+        cartBtn.innerText = "add to cart";
+        const productbox = document.createElement("div");
+        productbox.append(imgEl, titelEl, descriptionEl, ratingEl, stockEl, categoryEl, cartBtn);
+        showingResultDiv.append(productbox);
+    }
+} // function displaysearchedmovie(list) {
+ //     console.log(list);
+ //     showingResultsDiv.innerHTML = "";
+ //     if (list.results.length === 0) {
+ //       const h1El = document.createElement("h1");
+ //       h1El.innerText = "None found, check spelling";
+ //       showingResultsDiv.append(h1El);
+ //     } else {
+ //         boxDiv.append(posterImg, titelEl, aboutEl);
+ //         showingResultsDiv.append(boxDiv);
+ //       }
+ //     }
+ //   }
 
 },{}]},["dZI1r","jeorp"], "jeorp", "parcelRequire7992")
 
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,91 @@
-const baseurl:string = 'https://dummyjson.com/products'
+const baseurl: string = "https://dummyjson.com/products";
 fetch(baseurl)
-.then(res => res.json())
-.then(console.log);
+  .then((res) => res.json())
+  .then(console.log);
 
-
-const formEl= document.querySelector("form") as HTMLFormElement
+const formEl = document.querySelector("form") as HTMLFormElement;
 
 formEl.addEventListener("submit", handleform);
 async function handleform(event: Event) {
   event.preventDefault();
   console.log("i handleForm");
-  const userinput:string = (document.querySelector("input") as HTMLInputElement).value;
-  console.log(userinput)
-  searchresult(userinput)
-
-
-}    
-async function searchresult(userinput){
-let searchurl:string = baseurl + '/search?q=' + userinput
-const res= await fetch(searchurl)
-const data = await res.json()
-const products = data.products
-console.log(products)
+  const userinput: string = (
+    document.querySelector("input") as HTMLInputElement
+  ).value;
+  console.log(userinput);
+  searchresult(userinput);
 }
+async function searchresult(userinput) {
+  let searchurl: string = baseurl + "/search?q=" + userinput;
+  const res = await fetch(searchurl);
+  const data = await res.json();
+  const products = data.products;
+//   console.log(products);
+  display(products);
+}
+
+const showingResultDiv = document.querySelector(
+  "#showingResultDiv"
+) as HTMLDivElement;
+
+async function display(products) {
+  showingResultDiv.innerHTML = "";
+  console.log(products[0].title);
+
+    for(const product of products)
+    {console.log(product)
+    const img = product.images[0]
+    const title = product.title;
+    const description = product.description;
+    const rating = product.rating;
+    const stock = product.stock;
+    const category = product.category;
+
+    const imgEl= document.createElement('img')
+    const titelEl = document.createElement("h1");
+    const descriptionEl = document.createElement("p");
+    const ratingEl = document.createElement("p");
+    const stockEl = document.createElement("p");
+    const categoryEl = document.createElement("p");
+    const cartBtn = document.createElement("button");
+
+    imgEl.src = img
+    titelEl.innerText = title;
+    descriptionEl.innerText = description;
+    ratingEl.innerText = rating;
+
+    stockEl.innerText = stock;
+    if (stock <= 10) {
+      stockEl.innerText = stock + "(VARNING: few left)";
+    }
+
+    categoryEl.innerText = category;
+    cartBtn.innerText = "add to cart";
+
+    const productbox = document.createElement("div");
+    productbox.append(
+      imgEl,
+        titelEl,
+      descriptionEl,
+      ratingEl,
+      stockEl,
+      categoryEl,
+      cartBtn
+    );
+    showingResultDiv.append(productbox)
+  } 
+}
+
+// function displaysearchedmovie(list) {
+//     console.log(list);
+//     showingResultsDiv.innerHTML = "";
+//     if (list.results.length === 0) {
+//       const h1El = document.createElement("h1");
+//       h1El.innerText = "None found, check spelling";
+//       showingResultsDiv.append(h1El);
+//     } else {
+//         boxDiv.append(posterImg, titelEl, aboutEl);
+//         showingResultsDiv.append(boxDiv);
+//       }
+//     }
+//   }
